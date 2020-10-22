8addd54b18e8cd5fa3fd865e3bea160dc38ec08c
Chiranjeevi Thota <chiranjeevi.thota@leica-geosystems.com>
2020-10-22 13:54:46 +0200

    Client Roles: Enable OrderFrame only for paying users
    Provide TokenValidation options during configuration, with out it
    validation can't happen

--- src/ImageGallery.Client/Startup.cs
+++ src/ImageGallery.Client/Startup.cs
@@ -6,9 +6,11 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Net.Http.Headers;
 using System;
 using System.IdentityModel.Tokens.Jwt;
+using IdentityModel;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ImageGallery.Client
 {
@@ -79,6 +81,12 @@ namespace ImageGallery.Client
 					options.SaveTokens = true;
 					options.ClientSecret = "secret";
 					options.GetClaimsFromUserInfoEndpoint = true;
+
+					options.TokenValidationParameters = new TokenValidationParameters
+					{
+						NameClaimType = JwtClaimTypes.GivenName,
+						RoleClaimType = JwtClaimTypes.Role
+					};
 				});
 
 		}
